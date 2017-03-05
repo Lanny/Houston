fcd54180800464059f326a8d46f80e8c55dc204a
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-03-04 17:52:32 -0800

    Added admin.py

---
+++ Houston/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import *
+
+@admin.register(PageView)
+class PageViewAdmin(admin.ModelAdmin):
+    ordering = ('-report_time',)
+    list_display = ('user', 'path', 'report_time')
