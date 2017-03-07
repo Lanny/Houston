e1bd426fa65445bf6b7c399fbaeeff1c6fb68eb9
RyanJenkins <Ryan.Jenkins@wellsfargo.com>
2017-03-06 16:07:20 -0800

    More testsite urls so test data isn't so boring.

--- testsite/urls.py
+++ testsite/urls.py
@@ -3,8 +3,12 @@ from django.views.generic import TemplateView
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from testsite import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^houston/', include('Houston.urls')),
-    url(r'^$', TemplateView.as_view(template_name='testsite/index.html'))
+    url(r'^$', TemplateView.as_view(template_name='testsite/index.html')),
+    url(r'^pathing-test/(?P<pk_sorta_lol>\d+)/?$', views.pathed_view, 
+        name='pathed-view')
 ]
--- testsite/views.py
+++ testsite/views.py
@@ -1,3 +1,11 @@
+import random
+
 from django.shortcuts import render
 
-# Create your views here.
+def pathed_view(request, pk_sorta_lol):
+    ctx = {
+        'rand_pool': [random.randint(0,255) for _ in xrange(5)],
+        'thing': pk_sorta_lol
+    }
+
+    return render(request, 'testsite/pathed_template.html', ctx)
