2660e00f7ed0f7a06eb49adb586db27165805ba7
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-03-11 19:16:59 -0800

    Include gulp build step in distutils build process

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
 import os
+import subprocess
+import distutils
+
 from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+class CustomBuild(build_py):
+    def run(self):
+        self.run_command('gulp_build')
+        build_py.run(self)
+        print 'Done!'
+
+class GulpBuild(distutils.cmd.Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        assert os.path.exists('Houston/static/Houston/js/tracker.js')
+
+    def run(self):
+        wd = 'Houston/static-src'
+
+        self.announce('Running `npm install`', level=distutils.log.INFO)
+        cmd = subprocess.Popen(['npm', 'install'], cwd=wd)
+        cmd.wait()
+
+        self.announce('Running `gulp generate`', level=distutils.log.INFO)
+        cmd = subprocess.Popen(['gulp', 'generate', '--optimize'], cwd=wd)
+        cmd.wait()
 
 setup(
     name='django-houston',
@@ -32,4 +60,8 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    cmdclass={
+        'build_py': CustomBuild,
+        'gulp_build': GulpBuild
+    },
 )
